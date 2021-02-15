8031b553b0211cf0823156dae8d6180a3f9c17cd
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-15 09:15:44 +0900

    add admin boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_235930) do
+ActiveRecord::Schema.define(version: 2021_02_15_001503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_235930) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
   end
 
   create_table "volunteers", force: :cascade do |t|
