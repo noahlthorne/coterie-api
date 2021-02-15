f8e9e5f6a3c547d79cffab92588d4e9a95b8d07d
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-15 18:01:47 +0900

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Organization.destroy_all
+Volunteer.destroy_all
+
+# create admins
+ActiveRecord::Base.transaction do
+  20.times do
+    first_name = Faker::Name.name.split(' ')[0]
+    last_name = Faker::Name.name.split(' ')[1]
+    user = User.new(first_name: first_name, last_name: last_name, email: Faker::Internet.email, admin: true)
+    user.save!
+  end
+
+  # create normal users
+  200.times do
+    first_name = Faker::Name.name.split(' ')[0]
+    last_name = Faker::Name.name.split(' ')[1]
+    user = User.new(first_name: first_name, last_name: last_name, email: Faker::Internet.email, admin: false)
+    user.save!
+  end
+
+  # create organizations
+  20.times do
+    organization = Organization.new(name: Faker::Company.name)
+    organization.save!
+  end
+
+  # create volunteers
+  User.all.each do |user|
+    volunteer = Volunteer.new
+    volunteer.user = user
+    volunteer.organization = Organization.all.sample(rand(1..3)).first
+    volunteer.save!
+  end
+end
