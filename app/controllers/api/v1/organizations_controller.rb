1461bf0150e1bedf65abe22e3739730e9b9a9b29
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-16 12:30:44 +0900

    add organizaion controller actions

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,7 +1,40 @@
-class Api::V1:OrganizationsController < Api::V1::BaseController
+class Api::V1::OrganizationsController < Api::V1::BaseController
+  # acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_organization, only: [:show, :update]
+  # before_action :authenticate_user!
 
   def index
     @organizations = policy_scope(Organization)
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
 
+  def show
+  end
+
+
+  def update
+    if @organization.update(organization_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+  def set_organization
+    @organization = Organization.find(params[:id])
+    authorize @organization
+  end
+
+  def organization_params
+    params.require(:organization).permit(:name)
+  end
+
+  def render_error
+    render json: { errors: @organization.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
