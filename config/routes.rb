ec1038958181839d432d9d781e36fed23f15e798
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-16 12:32:19 +0900

    add show and update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :organizations, only: [ :index ] 
+      resources :organizations, only: [ :index, :show, :update ] 
     end
   end
 end
