4ebd17553a0ffc5e938a74fcc930260877c312e7
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-16 12:32:01 +0900

    update policies

--- app/policies/organization_policy.rb
+++ app/policies/organization_policy.rb
@@ -4,4 +4,15 @@ class OrganizationPolicy < ApplicationPolicy
       scope.all
     end
   end
+  
+  
+  def show?
+    true
+  end
+
+  def update?
+    true
+    # p user.
+    # record.users.find(user) == user && user.admin == true
+  end
 end
