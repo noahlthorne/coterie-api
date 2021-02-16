304570d8d836bb70efb786bf2291a00a7416abe3
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-16 12:31:45 +0900

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,29 @@ Organization.destroy_all
 Volunteer.destroy_all
 
 # create admins
+number_of_users = 0
 ActiveRecord::Base.transaction do
   20.times do
+    number_of_users += 1
     first_name = Faker::Name.name.split(' ')[0]
     last_name = Faker::Name.name.split(' ')[1]
     user = User.new(first_name: first_name, last_name: last_name, email: Faker::Internet.email, admin: true)
+    password = (0...8).map { (65 + rand(26)).chr }.join
+    user.password = password
     user.save!
+    p "#{number_of_users} users saved"
   end
 
   # create normal users
   200.times do
+    number_of_users += 1
     first_name = Faker::Name.name.split(' ')[0]
     last_name = Faker::Name.name.split(' ')[1]
     user = User.new(first_name: first_name, last_name: last_name, email: Faker::Internet.email, admin: false)
+    password = (0...8).map { (65 + rand(26)).chr }.join
+    user.password = password
     user.save!
+    p "#{number_of_users} users saved"
   end
 
   # create organizations
